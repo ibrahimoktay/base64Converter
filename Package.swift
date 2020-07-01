b9de3aedd1398890cdf19709efe8d33e43b39cad
ibrahim oktay <ibrahimoktay@yahoo.com>
2020-07-01 09:20:45 +0300

    Update library version

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .executable(name: "base64Converter", targets: ["base64Converter"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.0"),
     ],
     targets: [
         .target(
